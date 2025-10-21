c04dfd7176d333ee980eae6b874987b00705b715
Gilberto Silva <gilbertoantoniouk@gmail.com>
2025-10-21 18:41:41 +0100

    feat: add ValidationError class for handling validation errors

---
+++ backend/src/errors/validation.error.ts
@@ -0,0 +1,10 @@
+/*
+ * Custom Module
+ */
+import { BaseError } from "./base.error";
+
+export class ValidationError extends BaseError {
+  constructor(message: string) {
+    super("ValidationError", message, 400);
+  }
+}
